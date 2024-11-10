52bccab70a7df10c86e679615f427c44b1849205
muhilman2012 <muhi7714@gmail.com>
2024-11-10 20:45:36 +0700

    Adding Google Tag Manager

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -21,6 +21,14 @@
     <meta property="og:url" content="https://lapormaswapres.id/">
     <meta property="og:type" content="website">
     <meta property="og:site_name" content="Lapor Mas Wapres!">
+    <!-- Google Tag Manager -->
+    <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({
+        'gtm.start': new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5R6SXDK9');
+    </script>
+    <!-- End Google Tag Manager -->
 
     <style>
         :root {
@@ -51,6 +59,10 @@
 </head>
 
 <body class="bg-gray-50 text-gray-900">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5R6SXDK9"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <!-- Preloader -->
     <div id="preloader" role="status"
         class="flex z-30 h-screen w-full bg-white-25 items-center justify-center animate-pulse">
