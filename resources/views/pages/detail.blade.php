63600c39c6b85653b4b50d54ba2b5ef28d5ca8bb
regitaph <60743608+regitaph@users.noreply.github.com>
2024-11-10 14:12:41 +0700

    add s&k button and web icon

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -15,7 +15,7 @@
 
     <p class="text-sm sm:text-base text-red font-bold text-center max-w-screen-md">PERHATIAN !! kuota harian hanya 50 orang</p>
 
-    <div class="grid gap-y-4 sm:gap-y-5 w-full sm:grid-cols-1 md:grid-cols-2 gap-x-5 my-5 sm:my-[64px] max-w-[968px]">
+    <div class="grid gap-y-4 sm:gap-y-5 w-full sm:grid-cols-1 md:grid-cols-2 gap-x-5 my-5 sm:mt-[64px] max-w-[968px]">
         <div class="hover:cursor-pointer group flex gap-x-4 px-4 py-3 w-full bg-white transform hover:scale-105 transition duration-300 hover:shadow-custom hover:bg-gradient-to-r from-white to-secondary border-primary hover:border-secondary border rounded-lg items-center">
             <div class="flex items-center justify-center w-[32px] h-[28px] rounded-full bg-secondary text-primary font-bold text-base">1</div>
             <div class="flex flex-col w-full">
@@ -72,8 +72,16 @@
                 <p class="font-normal text-xs sm:text-sm text-primary">Pengadu dapat meninggalkan ruangan dumas lalu kembali ke lobby untuk menukar ID Tamu dengan kartu identitas, pengadu dapat mengecek status aduan pada akun lapor miliknya sesuai nomor registrasi yang diberi</p>
             </div>
         </div>
-        <!-- Tambah disini untuk button Syarat dan Ketentuan, lalu isi hrefnya menjadi href={{ route ('syarat') }} -->
+        
+        
     </div>
+    
+    <!-- Tambah disini untuk button Syarat dan Ketentuan, lalu isi hrefnya menjadi href={{ route ('syarat') }} -->
+    <div class="hover:cursor-pointer group px-4 py-3 mb-[64px] w-full bg-white transform hover:scale-105 transition duration-300 hover:shadow-custom hover:bg-gradient-to-r from-white to-secondary border-primary hover:border-secondary border rounded-lg items-center">
+            <p class="font-bold text-sm sm:text-sm text-primary text-center">Lihat Syarat dan Ketentuan</p>
+                
+    </div>
+    
 </div>
 @endsection
 
