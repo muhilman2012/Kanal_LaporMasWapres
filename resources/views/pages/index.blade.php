f83362c9f44169e34ca67dcb255c0f7b6688bfc9
muhilman2012 <muhi7714@gmail.com>
2024-11-17 07:15:08 +0700

    Update Alur v2

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -34,7 +34,7 @@
             <img class="w-5 h-5 sm:w-6 sm:h-6 group-hover:w-6 group-hover:h-6 sm:group-hover:w-7 sm:group-hover:h-7 transition-all duration-300" src="{{ url ('/assets/surat.webp') }}" alt="Surat">
             <div class="flex flex-col">
                 <p class="font-bold text-sm sm:text-base text-primary">Korespondensi Surat melalui Surel</p>
-                <p class="font-normal text-xs sm:text-sm text-primary">Kirimkan pengaduan surat melalui Surel</p>
+                <p class="font-normal text-xs sm:text-sm text-primary">Kirimkan pengaduan melalui Surel ke persuratan@set.wapresri.go.id.</p>
             </div>
         </a>
 
