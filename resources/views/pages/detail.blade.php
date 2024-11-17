529389f756e3b6f49969b4b0afd8e7523ecd973e
muhilman2012 <muhi7714@gmail.com>
2024-11-17 07:22:00 +0700

    Update Alur v2

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -152,7 +152,7 @@
         <div class="text-md text-dark-700 mb-4">
             <li><strong>KETENTUAN UMUM</strong>
                 <ol>
-                    <li>1. Pelayanan program <strong>"Lapor Mas Wapres!"</strong> diselenggarakan di Kantor Sekretariat Wakil Presiden, Jalan Kebon Sirih No.14 , Jakarta Pusat , pada :
+                    <li>1. Pelayanan program <strong>"Lapor Mas Wapres!"</strong> diselenggarakan di Kantor Sekretariat Wakil Presiden, Jalan Kebon Sirih No.14 , Jakarta Pusat , pada hari kerja :
                         <ul>
                             <li>a. Senin s.d. Kamis pukul 08.00 s.d. 14.00 WIB (istirahat 12.00 s.d. 13.00 WIB)</li>
                             <li>b. Jumat pukul 08.00 s.d. 14.30 WIB (istirahat 11.00 s.d.13.30 WIB)</li>
