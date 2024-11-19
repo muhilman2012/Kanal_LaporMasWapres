93690af287cc10d7b30b0e48bd16f9611018c2e7
muhilman2012 <muhi7714@gmail.com>
2024-11-19 13:48:02 +0700

    Delete Tata Tertib

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -127,98 +127,12 @@
         </div>
     </div>
 
-    <a onclick="openModal()" class="w-full bg-gradient-to-r from-primary to-secondary text-white text-center font-semibold text-sm py-3 rounded-lg transform hover:scale-105 transition duration-300 hover:shadow-custom cursor-pointer">
+    <a href="{{ route('reservasi') }}" class="w-full bg-gradient-to-r from-primary to-secondary text-white text-center font-semibold text-sm py-3 rounded-lg transform hover:scale-105 transition duration-300 hover:shadow-custom cursor-pointer">
         <p class="font-bold text-lg sm:text-lg text-white text-center">Registrasi <i>Online</i> (klik di sini) <br> (kuota <i>online</i> 50 orang/hari)</p>
     </a>
-</div>
-<!-- Modal untuk Syarat dan Ketentuan -->
-<div id="modal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 hidden z-50">
-    <div class="bg-white p-6 rounded-lg w-full max-w-md max-h-[90vh] overflow-y-auto">
-        <h2 class="text-xl font-bold mb-4 text-center">Tata Tertib Pelayanan Program <br> Lapor Mas Wapres!</h2>
-        <div class="text-md text-dark-700 mb-4">
-            <li><strong>KETENTUAN UMUM</strong>
-                <ol>
-                    <li>1. Pelayanan program <strong>Lapor Mas Wapres!</strong> diselenggarakan di Kantor Sekretariat Wakil Presiden, Jalan Kebon Sirih No. 14, Jakarta Pusat, pada hari kerja:
-                        <ul>
-                            <li>a. Senin s.d. Kamis, pukul 08.00 s.d. 14.00 WIB (istirahat, pukul 12.00 s.d. 13.00 WIB)</li>
-                            <li>b. Jumat, pukul 08.00 s.d. 14.30 WIB (istirahat, pukul 11.00 s.d. 13.30 WIB)</li>
-                        </ul>
-                    </li>
-                    <li>2. Pelapor memakai pakaian bebas rapi.</li>
-                    <li>3. Pelapor wajib membawa kartu identitas (KTP/SIM/Identitas lain yang tercantum NIK).</li>
-                    <li>4. Pengaduan yang dilayani berjumlah maksimal 50 orang/hari.</li>
-                </ol>
-            </li><br>
-
-            <li><strong>KETENTUAN PENGADUAN</strong>
-                <ol>
-                    <li>1. Pelapor adalah orang yang langsung mengalami kejadian. Apabila pelapor bukan yang mengalami kejadian langsung, maka pelapor harus membawa surat kuasa bermaterai dari pihak yang diwakili.</li>
-                    <li>2. Substansi aduan tidak sedang atau telah menjadi objek peradilan.</li>
-                    <li>3. Substansi aduan belum pernah disampaikan oleh pelapor kepada Wakil Presiden.</li>
-                    <li>4. Pelapor wajib membawa dokumen pendukung pengaduan yang lengkap dan relevan.</li>
-                    <li>5. Petugas memverifikasi dokumen pengaduan. Apabila dokumen tidak lengkap, petugas akan meminta pelapor untuk mengirimkan kelengkapan dokumen melalui surel <i>lapormaswapres@set.wapresri.go.id</i> Pelaporan tidak diproses apabila pelapor tidak melengkapi dokumen tersebut dalam kurun waktu 10 hari.</li>
-                    <li>6. Pelapor wajib menyampaikan nomor kontak atau surel yang dapat dihubungi.</li>
-                </ol>
-            </li><br>
-
-            <li><strong>REGISTRASI DAN PROSES PENGADUAN</strong>
-                <ol>
-                    <li>1. Pelapor melakukan registrasi secara <i>online</i> melalui <a href="https://lapormaswapres.id"><i>https://lapormaswapres.id</i></a></li>
-                    <li>2. Pelapor yang telah berhasil melakukan registrasi <i>online</i>, harap hadir sesuai tanggal yang dipilih.</li>
-                    <li>3. Pelapor menunggu di ruang tunggu yang telah disediakan.</li>
-                    <li>4. Petugas memverifikasi dan memberikan nomor antrian pengaduan.</li>
-                    <li>5. Petugas mempersilahkan pelapor ke Ruang Pengaduan berdasarkan nomor antrian.</li>
-                </ol>
-            </li><br>
-
-            <li><strong>HAL-HAL LAIN</strong>
-                <ol>
-                    <li>1. Pelapor menghormati tata tertib yang berlaku, menjaga etika dan kesopanan selama berada di lingkungan Sekretariat Wakil Presiden.</li>
-                    <li>2. Pelapor dilarang mengambil gambar/video dan membuat konten selama proses pelaporan.</li>
-                    <li>3. Pelapor harus menaati seluruh ketentuan dalam Tata Tertib <b>Lapor Mas Wapres!</b> dan ketentuan lain yang ditetapkan di kemudian hari.</li>
-                </ol>
-            </li>
-        </div>
-
-        <!-- Checkbox untuk persetujuan syarat dan ketentuan -->
-        <div class="flex items-center mb-4">
-            <input type="checkbox" id="agreeCheckbox" class="mr-2" onchange="toggleRegisterButton()">
-            <label for="agreeCheckbox" class="text-sm">Setuju, Saya telah membaca seluruh Syarat dan Ketentuan yang berlaku</label>
-        </div>
-
-        <!-- Tombol Registrasi Sekarang -->
-        <a id="registerButton" href="{{ route('reservasi') }}" class="bg-primary text-white py-2 px-4 rounded-lg text-center w-full block cursor-not-allowed opacity-50 pointer-events-none">
-            Registrasi Sekarang
-        </a>
-
-        <!-- Tombol Tutup Modal -->
-        <button onclick="closeModal()" class="mt-4 text-primary font-semibold text-sm underline w-full text-center">Tutup</button>
-    </div>
 </div>
 @endsection
 
 @section('script')
-<script>
-    // Fungsi untuk membuka modal
-    function openModal() {
-        document.getElementById('modal').classList.remove('hidden');
-    }
 
-    // Fungsi untuk menutup modal
-    function closeModal() {
-        document.getElementById('modal').classList.add('hidden');
-    }
-
-    // Fungsi untuk mengaktifkan/menonaktifkan tombol registrasi
-    function toggleRegisterButton() {
-        const checkbox = document.getElementById('agreeCheckbox');
-        const registerButton = document.getElementById('registerButton');
-        
-        if (checkbox.checked) {
-            registerButton.classList.remove('cursor-not-allowed', 'opacity-50', 'pointer-events-none');
-        } else {
-            registerButton.classList.add('cursor-not-allowed', 'opacity-50', 'pointer-events-none');
-        }
-    }
-</script>
 @endsection
