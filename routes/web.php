928a02bfc8a0cbf20aa37dfd4a6fc695eb813628
muhilman2012 <muhi7714@gmail.com>
2024-11-21 08:26:45 +0700

    Update Sitemap

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\pages\indexController;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +21,18 @@ Route::get('/', [indexController::class, 'index'])->name('index');
 Route::get('/tatap-muka', [indexController::class, 'detail'])->name('detail');
 Route::get('/syarat-ketentuan', [indexController::class, 'syarat'])->name('syarat');
 Route::get('/registrasi', [indexController::class, 'reservasi'])->name('reservasi');
+
+Route::get('/sitemap.xml', function () {
+    $sitemap = Sitemap::create()
+        ->add(Url::create('/')
+            ->setPriority(1.0)
+            ->setChangeFrequency('daily'))
+        ->add(Url::create('/tatap-muka')
+            ->setPriority(0.8)
+            ->setChangeFrequency('weekly'))
+        ->add(Url::create('/registrasi')
+            ->setPriority(0.9)
+            ->setChangeFrequency('weekly'));
+
+    return $sitemap->toResponse(request());
+});
