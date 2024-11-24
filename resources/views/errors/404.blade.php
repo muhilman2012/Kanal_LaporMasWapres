7cab061a05dcc70200319dd94caa2a0ead0e4a3a
regitaph <60743608+regitaph@users.noreply.github.com>
2024-11-24 17:29:22 +0700

    404 page update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,13 +9,13 @@
             flex-direction: column;
             align-items: center;
             justify-content: center;
-            height: calc(100vh - 150px); /* Adjust as needed */
+            /* height: calc(100vh - 150px); Adjust as needed */
         }
 
         .error-code {
-            font-size: 4rem;
+            font-size: 6rem;
             font-weight: bold;
-            color: #e74c3c; /* Example error color */
+            /* color: #e74c3c; Example error color */
             margin-bottom: 1rem;
         }
 
@@ -28,9 +28,19 @@
 @endsection
 
 @section('pages')
-<div class="error-container">
-    <div class="error-code">404</div>
-    <div class="error-message">Halaman Tidak Ditemukan</div>
-    <a href="{{ route('index') }}" class="btn btn-primary">Kembali ke Beranda</a>
+<div class="flex flex-1 justify-center w-full" style="padding-top: 180px;">
+    <div class="relative error-container w-[625px] bg-sky gap-y-5 rounded-3xl">
+        <div class="absolute error-code " style="top: -185px;">
+            <img src="{{ url ('/assets/404.png') }}" alt="">
+        </div>
+        <div class="flex flex-col mb-0 gap-y-5 error-message text-center" style="padding-top: 40px;">
+            <p class="text-6xl font-bold text-black">Oops<span class="italic">!</span></p>
+            <p>Halaman Tidak Ditemukan</p>
+            <a href="{{ route('index') }}" class="bg-gradient-to-r from-primary to-secondary text-white text-center font-semibold text-sm border-none py-3 px-4 rounded-lg transform hover:scale-105 transition duration-300 hover:shadow-custom cursor-pointer">Kembali ke Beranda</a>
+        </div>
+
+    </div>
 </div>
+
+
 @endsection
